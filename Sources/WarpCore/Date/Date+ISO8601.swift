a4882de5b63e949fc67b68856e3636ef13fa9606
Jeremy Jacobson <jjacobson93@gmail.com>
2017-03-27 19:55:39 -0700

    Fix issue with Date and ISO8601 on Linux

--- Sources/WarpCore/Date/Date+ISO8601.swift
+++ Sources/WarpCore/Date/Date+ISO8601.swift
@@ -9,12 +9,15 @@
 import Foundation
 
 extension Date {
-    public var iso8601: String {
+    fileprivate static let iso8601Formatter: DateFormatter = { _ in
         let formatter = DateFormatter()
-        formatter.calendar = Calendar(identifier: .iso8601)
         formatter.locale = Locale(identifier: "en_US_POSIX")
         formatter.timeZone = TimeZone(secondsFromGMT: 0)
         formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssXXXXX"
-        return formatter.string(from: self)
+        return formatter
+    }()
+    
+    public var iso8601: String {
+        return Date.iso8601Formatter.string(from: self)
     }
 }
